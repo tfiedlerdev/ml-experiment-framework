4d629bf0c239e72892576690270a6f98214d83b6
tfiedlerdev <tobias.fiedler@student.hpi.de>
2024-11-06 14:06:17 +0100

    Change NamedTuple to dataclass for better subclassing

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -5,36 +5,36 @@ from typing_extensions import Self
 import torch
 from torch.utils.data import Dataset
 from torch._prims_common import DeviceLikeType
+from dataclasses import dataclass
 
 
-class Sample(NamedTuple):
+@dataclass
+class Sample:
     input: torch.Tensor
     target: Any
 
 
-class Batch(NamedTuple):
+@dataclass
+class Batch:
     input: torch.Tensor
     target: Optional[torch.Tensor]
 
     def to(self, device: DeviceLikeType):
-        copy = self._replace(
-            input=self.input.to(device),
-            target=self.target.to(device) if self.target is not None else None,
-        )
+        self.input = self.input.to(device)
+        self.target = self.target.to(device) if self.target is not None else None
+
         if hasattr(self, "__dict__"):
             # Putting all tensors of potential subclass attributes to device
             for key, value in self.__dict__.items():
                 if isinstance(value, torch.Tensor):
-                    copy.__setattr__(key, value.to(device))
-                else:
-                    copy.__setattr__(key, value)
-        return copy
+                    self.__setattr__(key, value.to(device))
+        return self
 
     def cuda(self):
         return self.to("cuda")
 
     def copy_and_change(self, **diff):
-        copy = self._replace(**diff)
+        copy = self.__class__(**diff)
         for key, value in self.__dict__.items():
             copy.__setattr__(key, value)
 
