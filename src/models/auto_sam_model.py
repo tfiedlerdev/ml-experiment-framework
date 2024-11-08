7e48ed1f81f05115d4f64a86b70206d07ba82c4e
tfiedlerdev <tobias.fiedler@student.hpi.de>
2024-11-08 14:35:57 +0100

    Track dice and IoU scores and add high level image segmentation method

--- src/models/auto_sam_model.py
+++ src/models/auto_sam_model.py
@@ -14,6 +14,18 @@ from pydantic import BaseModel as PDBaseModel
 from torch.nn import BCELoss
 from src.models.auto_sam_prompt_encoder.model_single import ModelEmb
 from torch.nn import functional as F
+import numpy as np
+
+
+def get_dice_ji(predict, target):
+    predict = predict + 1
+    target = target + 1
+    tp = np.sum(((predict == 2) * (target == 2)) * (target > 0))
+    fp = np.sum(((predict == 2) * (target == 1)) * (target > 0))
+    fn = np.sum(((predict == 1) * (target == 2)) * (target > 0))
+    ji = float(np.nan_to_num(tp / (tp + fp + fn)))
+    dice = float(np.nan_to_num(2 * tp / (2 * tp + fp + fn)))
+    return dice, ji
 
 
 class AutoSamModelArgs(PDBaseModel):
@@ -33,6 +45,7 @@ sam_model_registry = {
 }
 
 
+# Source of most of this code: https://github.com/talshaharabany/AutoSAM
 class AutoSamModel(BaseModel[RetinaBatch]):
     def __init__(self, config: AutoSamModelArgs):
         super().__init__()
@@ -60,26 +73,119 @@ class AutoSamModel(BaseModel[RetinaBatch]):
 
         return ModelOutput(masks)
 
-    def compute_loss(self, outputs: ModelOutput, batch: Batch) -> Loss:
+    def compute_loss(self, outputs: ModelOutput, batch: RetinaBatch) -> Loss:
         assert batch.target is not None
         size = outputs.logits.shape[2:]
         gts_sized = F.interpolate(batch.target.unsqueeze(dim=1), size, mode="nearest")
 
         bce = self.bce_loss.forward(outputs.logits, gts_sized)
-        dice = dice_loss(outputs.logits, gts_sized)
-        loss_value = bce + dice
+        dice_loss = compute_dice_loss(outputs.logits, gts_sized)
+        loss_value = bce + dice_loss
+
+        input_size = tuple([int(x) for x in batch.image_size[0].squeeze().tolist()])
+        original_size = tuple(
+            [int(x) for x in batch.original_size[0].squeeze().tolist()]
+        )
+        masks = self.sam.postprocess_masks(
+            outputs.logits, input_size=input_size, original_size=original_size
+        )
+        gts = self.sam.postprocess_masks(
+            batch.target.unsqueeze(dim=0),
+            input_size=input_size,
+            original_size=original_size,
+        )
+        masks = F.interpolate(
+            masks,
+            (self.config.Idim, self.config.Idim),
+            mode="bilinear",
+            align_corners=True,
+        )
+        gts = F.interpolate(gts, (self.config.Idim, self.config.Idim), mode="nearest")
+        masks[masks > 0.5] = 1
+        masks[masks <= 0.5] = 0
+        dice_score, IoU = get_dice_ji(
+            masks.squeeze().detach().cpu().numpy(), gts.squeeze().detach().cpu().numpy()
+        )
 
         return Loss(
             loss_value,
             {
                 "dice+bce_loss": loss_value.detach().item(),
-                "dice_loss": dice.detach().item(),
+                "dice_loss": dice_loss.detach().item(),
                 "bce_loss": bce.detach().item(),
+                "dice_score": dice_score,
+                "IoU": IoU,
             },
         )
 
+    def segment_image(self, image: np.ndarray):
+        import cv2
+        from .segment_anything.utils.transforms import ResizeLongestSide
 
-def dice_loss(y_true, y_pred, smooth=1):
+        def sam_call(batched_input, sam, dense_embeddings):
+            with torch.no_grad():
+                input_images = torch.stack(
+                    [sam.preprocess(x) for x in batched_input], dim=0
+                )
+                image_embeddings = sam.image_encoder(input_images)
+                sparse_embeddings_none, dense_embeddings_none = sam.prompt_encoder(
+                    points=None, boxes=None, masks=None
+                )
+
+                low_res_masks, iou_predictions = sam.mask_decoder(
+                    image_embeddings=image_embeddings,
+                    image_pe=sam.prompt_encoder.get_dense_pe(),
+                    sparse_prompt_embeddings=sparse_embeddings_none,
+                    dense_prompt_embeddings=dense_embeddings,
+                    multimask_output=False,
+                )
+            return low_res_masks
+
+        original_size = image.shape[:2]
+        transform = ResizeLongestSide(1024)
+        Idim = 512
+        image_tensor = torch.tensor(image).permute(2, 0, 1).float() / 255.0
+        image_tensor = transform.apply_image_torch(image_tensor)
+
+        input_images = transform.preprocess(image_tensor).unsqueeze(dim=0)
+        input_images = image_tensor.unsqueeze(dim=0).cuda()
+
+        orig_imgs_small = F.interpolate(
+            input_images, (Idim, Idim), mode="bilinear", align_corners=True
+        )
+        dense_embeddings = self.prompt_encoder.forward(orig_imgs_small)
+
+        mask = sam_call(input_images, self.sam, dense_embeddings)
+        input_size = image_tensor.shape[1:]
+        mask = self.sam.postprocess_masks(
+            mask, input_size=input_size, original_size=original_size
+        )
+        mask = mask.squeeze().cpu().numpy()
+        mask = (mask - mask.min()) / (mask.max() - mask.min())
+        mask = (255 * mask).astype(np.uint8)
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        return image, mask
+
+    def segment_image_from_file(self, image_path: str):
+        import cv2
+
+        image = cv2.imread(image_path)
+        return self.segment_image(image)
+
+    def segment_and_write_image_from_file(
+        self, image_path: str, output_path: str, mask_opacity: float = 0.4
+    ):
+        import cv2
+
+        image, mask = self.segment_image_from_file(image_path)
+        overlay = (np.array(mask) * np.array([1, 1, 0])).astype(image.dtype)
+        output_image = cv2.addWeighted(
+            image, 1 - mask_opacity, overlay, mask_opacity, 0
+        )
+        cv2.imwrite(output_path, output_image)
+
+
+def compute_dice_loss(y_true, y_pred, smooth=1):
     alpha = 0.5
     beta = 0.5
     tp = torch.sum(y_true * y_pred, dim=(1, 2, 3))
