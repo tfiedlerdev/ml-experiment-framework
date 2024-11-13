4d82b72c5e2bf943835919bca1c091db410f3a41
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2024-11-13 13:53:29 +0000

    test visualizations

--- src/datasets/filter_dataset.py
+++ src/datasets/filter_dataset.py
@@ -21,6 +21,17 @@ from torchvision import transforms
 class FilterFileReference:
     img_path: str
     label: int
+    test: bool
+
+
+@dataclass
+class FilterSample(Sample):
+    file_path: str
+
+
+@dataclass
+class FilterBatch(Batch):
+    file_paths: list[str]
 
 
 class FilterDatasetArgs(BaseModel):
@@ -41,7 +52,7 @@ class FilterDataset(BaseDataset):
         self.config = config
         self.samples = self.load_data() if samples is None else samples
 
-    def __getitem__(self, index: int) -> Sample:
+    def __getitem__(self, index: int) -> FilterSample:
         sample = self.samples[index]
 
         image = Image.open(sample.img_path)
@@ -59,40 +70,42 @@ class FilterDataset(BaseDataset):
 
         preprocess_img = preprocess(image)
 
-        return Sample(
+        return FilterSample(
             input=torch.Tensor(preprocess_img),
             target=torch.Tensor([sample.label == 0, sample.label == 1]),
+            file_path=sample.img_path,
         )
 
     def __len__(self):
         return len(self.samples)
 
     def get_collate_fn(self):  # type: ignore
-        def collate(samples: list[Sample]):
+        def collate(samples: list[FilterSample]):
             inputs = torch.stack([s.input for s in samples])
             targets = torch.stack([s.target for s in samples])
-            return Batch(inputs, targets)
+            file_paths = [s.file_path for s in samples]
+            return FilterBatch(inputs, targets, file_paths)
 
         return collate
 
     def get_split(self, split: Literal["train", "val", "test"]) -> Self:
-        seed(42)
-        shuffle(self.samples)
-        index_offset = (
-            0
-            if split == "train"
-            else floor(len(self.samples) * self.config.train_percentage)
-        )
-        length = (
-            floor(len(self.samples) * self.config.train_percentage)
-            if split == "train"
-            else floor(len(self.samples) * self.config.val_percentage)
-        )
-
+        if split != "test":
+            train_val_samples = [sample for sample in self.samples if not sample.test]
+            samples = (
+                train_val_samples[
+                    : floor(len(train_val_samples) * self.config.train_percentage)
+                ]
+                if split == "train"
+                else self.samples[
+                    floor(len(train_val_samples) * self.config.train_percentage) :
+                ]
+            )
+        else:
+            samples = [sample for sample in self.samples if sample.test]
         return self.__class__(
             self.config,
             self.yaml_config,
-            self.samples[index_offset : index_offset + length],
+            samples,
         )
 
     def load_data(self) -> list[FilterFileReference]:
@@ -100,14 +113,36 @@ class FilterDataset(BaseDataset):
 
         good_sample_filepath = os.path.join(dir, "good.txt")
         bad_sample_filepath = os.path.join(dir, "bad.txt")
+        test_sample_filepath = os.path.join(dir, "good_biobank.txt")
 
-        file_samples = []
+        train_val_samples = []
+        test_file_samples = []
 
-        for i, path in enumerate([bad_sample_filepath, good_sample_filepath]):
-            with open(path, "r") as f:
-                sample_lines = f.readlines()
-                for line in sample_lines:
-                    line = line.strip()
-                    file_samples.append(FilterFileReference(line, i))
+        with open(good_sample_filepath, "r") as f:
+            sample_lines = f.readlines()
 
-        return file_samples
+            for line in sample_lines:
+                line = line.strip()
+                train_val_samples.append(FilterFileReference(line, 1, False))
+
+        with open(bad_sample_filepath, "r") as f:
+            sample_lines = f.readlines()
+
+            for i, line in enumerate(sample_lines):
+                line = line.strip()
+                is_test = i > len(sample_lines) - 20
+                if is_test:
+                    test_file_samples.append(FilterFileReference(line, 0, True))
+                else:
+                    train_val_samples.append(FilterFileReference(line, 0, False))
+
+        with open(test_sample_filepath, "r") as f:
+            sample_lines = f.readlines()
+
+            for line in sample_lines:
+                line = line.strip()
+                test_file_samples.append(FilterFileReference(line, 1, True))
+
+        seed(42)
+        shuffle(train_val_samples)
+        return train_val_samples + test_file_samples
--- src/experiments/resnet_filter_experiment.py
+++ src/experiments/resnet_filter_experiment.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from typing import Literal, Any
 import torch
 from torch.optim.optimizer import Optimizer
@@ -9,6 +10,8 @@ from src.optimizers.adam import create_adam_optimizer, AdamArgs
 from src.schedulers.step_lr import StepLRArgs, create_steplr_scheduler
 from src.models.resnet_filter_model import ResnetFilterModel, ResnetFilterModelArgs
 from src.datasets.filter_dataset import FilterDataset, FilterDatasetArgs
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
 class ResnetFilterExperimentArgs(
@@ -52,3 +55,55 @@ class ResnetFilterExperiment(BaseExperiment):
 
     def get_loss_name(self) -> str:
         return "bce_loss"
+
+    def run_after_training(self, trained_model: BaseModel):
+        self.model.eval()
+        test_loader = self._create_dataloader(split="test")
+
+        all_preds = []
+        all_labels = []
+
+        fps = []
+        fns = []
+
+        with torch.no_grad():
+            for data in test_loader:
+                outputs = trained_model(data.cuda())
+                preds = torch.argmax(outputs.logits, dim=1)
+
+                for i, filepath in enumerate(data.file_paths):
+                    pred = preds[i]
+                    prob = outputs.logits[i].softmax(0)
+                    label = data.target[i].argmax(0)
+
+                    if pred == 1 and label == 0:
+                        fps.append((filepath, prob[0].item(), prob[1].item()))
+                    elif pred == 0 and label == 1:
+                        fns.append((filepath, prob[0].item(), prob[1].item()))
+
+                all_preds.extend(preds.cpu().numpy())
+                all_labels.extend(data.target.argmax(1).cpu().numpy())
+
+        cm = confusion_matrix(all_labels, all_preds)
+        plt.figure(figsize=(10, 7))
+        sns.heatmap(
+            cm,
+            annot=True,
+            fmt="d",
+            cmap="Blues",
+            xticklabels=["0", "1"],
+            yticklabels=["0", "1"],
+        )
+        plt.xlabel("Predicted")
+        plt.ylabel("True")
+        plt.title(f"Confusion Matrix, {len(all_preds)} total samples")
+        plt.savefig(self.results_dir + "/confusion_matrix.png")
+
+        with open(self.results_dir + "/fps.csv", "w") as f:
+            f.write("file_path,neg_prob,pos_prob\n")
+            for fp in fps:
+                f.write(f"{fp[0]},{fp[1]},{fp[2]}\n")
+        with open(self.results_dir + "/fns.csv", "w") as f:
+            f.write("file_path,neg_prob,pos_prob\n")
+            for fn in fns:
+                f.write(f"{fn[0]},{fn[1]},{fn[2]}\n")
